3eb77ee1c30776eeb5011b43f99278627ddc3043
Jürgen Kober <mindeleven@posteo.de>
2020-07-06 12:02:43 +0200

    Added constructor to UsersRepository to create a users file; users.json file created by script.

--- repositories/users.js
+++ repositories/users.js
@@ -1,3 +1,19 @@
+const fs = require('fs');
+
 class UsersRepository {
-  
+  constructor(filename) {
+    if (!filename) {
+      throw new Error('Creating a repository requires a filename!')
+    }
+
+    this.filename = filename;
+    try {
+      fs.accessSync(this.filename);
+    } catch {
+      fs.writeFileSync(this.filename, '[]');
+    }
+
+  }
 }
+
+new UsersRepository('users.json');
