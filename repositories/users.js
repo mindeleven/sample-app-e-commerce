73803576ae531589c8faaacbf709ffe1dc1c104d
Jürgen Kober <mindeleven@posteo.de>
2020-07-17 11:13:10 +0200

    Implemented the update method.

--- repositories/users.js
+++ repositories/users.js
@@ -45,20 +45,32 @@ class UsersRepository {
     const filteredRecords = records.filter(record => record.id !== id);
     await this.writeAll(filteredRecords);
   }
+
+  async update(id, attrs) {
+    const records = await this.getAll();
+    const record = records.find(record => record.id === id);
+    if (!record) {
+      throw new Error(`Record with id ${id} not found`);
+    }
+
+    Object.assign(record, attrs);
+    await this.writeAll(records);
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
-  /***
   let date = new Date(); let timestamp = date. getTime();
+  /***
   await repo.create({
-    'email': `test_${timestamp}@test.com`,
-    'password': `password${timestamp}`
+    'email': `test_${timestamp}@test.com`
+    // 'password': `password${timestamp}`
   });
-  const users = await repo.getAll();
   ***/
+  //const users = await repo.getAll();
+  await repo.update('45dd8b98', { 'password': `password${timestamp}` });
   //const user = await repo.getOne('53a45fca');
-  await repo.delete('53a45fca');
+  // await repo.delete('53a45fca');
   //console.log(users);
   //console.log(user);
 }
