bbbf3b6ef003411db3b64142c44dab6751a804d5
Jürgen Kober <mindeleven@posteo.de>
2020-07-22 19:43:39 +0200

    Wiring up the auth routes with the app through the index file.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
-const userRepo = require('./repositories/users');
+const authRouter = require('./routes/admin/auth');
 
 const app = express();
 
@@ -10,6 +10,8 @@ app.use(bodyParser.urlencoded({ extended: 'true' }));
 app.use(cookieSession({
   keys: ['SFosFt6DakMksn3ng3II']
 }));
+
+app.use(authRouter);
 
 app.listen(3000, () => {
   console.log('listening to port 3000...');
